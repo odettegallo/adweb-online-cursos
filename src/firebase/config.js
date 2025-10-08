b546819c465d62dc378be14cb2ddc4854786e8dc
Odette Gallo <odette.gallo@hotmail.com>
2025-10-08 20:00:29 -0300

    carpeta firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// src/firebase/config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // Tus credenciales aquí
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
