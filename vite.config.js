3e108f6125f21c856a6fd295d72ee662425ba125
aeliasho <antonioeliashoshee@gmail.com>
2025-10-21 18:53:39 -0300

    feat: Configuración de Vitest e implementación de pruebas unitarias para Navbar y UserCoursesList

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.js
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,6 +16,24 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  test: {
+    environment: 'jsdom', 
+    
+   
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped', 
+      },
+    },
+
+    
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
     },
   },
 })
